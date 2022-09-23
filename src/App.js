c4099f6e58c3ff744f39652a9dd21f1d1bdf201f
adithya <madithyam16@gmail.com>
2022-09-23 23:26:28 +0530

    akskdnkjasn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 
 import { Ground } from './component/Ground';
+import { Player } from './component/Player';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
         <Physics>
+          <Player />
           <Ground />
         </Physics>
       </Canvas>
