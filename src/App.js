2c8ba023b41a2f1c81d469f4c5ac2f531d21eb4a
adithya <madithyam16@gmail.com>
2022-09-23 21:53:37 +0530

    ground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+
+import { Ground } from './component/Ground';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
+      {/* <div>Outside Canvas</div> */}
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
